4a60ab3d0e09327cdc76eca58a12b0aa40f41856
fady emad <femad7732@gmail.con>
2024-08-13 18:16:28 +0300

    demo

--- Program.cs
+++ Program.cs
@@ -1,86 +1,75 @@
-﻿
-using ASSI_2_C__ADVANCED;
-using System.Collections;
+﻿namespace demo_c__advanced
 
-internal class prog
+{
+    internal class prog
     {
-    static void Main(string[] args)
-    {
-        #region part 1
-
-        // non generic colliction => array list (Heterogeneous) : have (hashcode,lists) , count , capacity , 
-        // generic collection => list (dynamic size , Homogeneous) : have (hashcode,lists) , fast to retrive
-        // => linkedlist (dynamic size , Homogeneous) : have nodes , fast to add
-        // queue : first in first out
-        //stack : last in first out
-        #endregion
-
-        #region part 2
-        #region q1
-     static void rev( ArrayList arrayList)
-    {
-        object temp;
-        int end = arrayList.Count;
-        for (int i = 0; i < arrayList.Count / 2; i++)
+        static int sumlist(List<int> num)
         {
-            temp = arrayList[i];
-            arrayList[i] = arrayList[end - 1];
-            arrayList[end - 1] = temp;
-            end--;
-        }
-    }
-      
-        ArrayList arr1 = new ArrayList();
-        arr1.AddRange(new int [] {1, 2, 3});
-        foreach (int item in arr1)
-        {
-        Console.WriteLine(item);
-        }
-        rev(arr1);
-        Console.WriteLine("after reverse");
-        foreach (int item in arr1)
-        {
-            Console.WriteLine(item);
-        }
-        #endregion
-        #region q2
-        Console.WriteLine("=======================");
-        static List<int> even_num(List<int> l1)
-        {
-            int counter = 0;
-            List<int> l2 = new List<int>() ;
-            while (counter < l1.Count)
+            int sum = 0;
+            foreach (int item in num)
             {
-                if (l1[counter] %2 ==0)
-                {
-                    l2.Add ( l1[counter]);
-                }
-                counter++;
+                sum += item;
             }
-            return l2;
+            return sum;
         }
-        List <int> list = new List<int>() { 1,2 ,3,4,5,6,7,8,9,10,11,12};
-        List<int> l = even_num(list); 
-        foreach (int item in l)
+
+
+        static void Main(string[] args)
         {
-            Console.WriteLine(item);
+            #region generic
+            //List<int> num = new List<int>();
+            //Console.WriteLine($" count = {num.Count}");
+            //Console.WriteLine($"capacity = {num.Capacity}");
+            //num.Add(1);
+            //num.Add(2);
+            //num.Add(3);
+            //num.Add(4);
+            //num.Add(5);
+            //num.Add(4);
+            //num.AddRange([1, 2, 3]);
+            //num.TrimExcess();
+            //Console.WriteLine($" count = {num.Count}");
+            //Console.WriteLine($"capacity = {num.Capacity}");
+            //foreach(int item in num)
+            //{
+            //    Console.Write(item);    
+            //}
+            //Console.WriteLine("=====================");
+            //int res= sumlist(num);
+            //Console.WriteLine(res);
+            //num.Insert(3, 6);
+            //foreach (int item in num)
+            //{
+            //    Console.Write(item);
+            //}
+            //Console.WriteLine("==========");
+            //Console.WriteLine(num.IndexOf(3));
+            //num.Remove(3);
+            //foreach (int item in num)
+            //{
+            //    Console.Write(item);
+            //}
+            //LinkedList<int> linked = new LinkedList<int>();
+            //linked.AddFirst(1);
+            //linked.AddLast(2);
+            //foreach(int item in linked)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //LinkedListNode <int> x = linked.Find(1);
+            //linked.AddAfter(x, 10);
+            //Console.WriteLine("==================");
+            //foreach (int item in linked)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //stack.Push(4);
+            //stack.Pop();
+            #endregion
         }
-
-        #region q3
-        fixedsizelist<int> list1 = new fixedsizelist<int>(5);
-        list1.add(1);
-        list1.add (2);
-        list1.add(3);
-        list1.add(4);
-        list1.add(5);
-        list1.get(0);
-        list1.get(5);
-        #endregion
-
-        #endregion
-
-        #endregion
-
-
     }
 }
